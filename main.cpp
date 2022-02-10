58b2eff1a94bee6a844991a25a9eca610629e744
Łukasz <lukasz@pyszny.eu>
2022-02-10 05:24:53 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include "spis.h"
+
+using namespace std;
+
+int main()
+{
+    //test_pliku("rekordy.txt");
+    //k_p_n();
+    //k_i_k();
+    glowne_menu();
+    return 0;
+}
+
